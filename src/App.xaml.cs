f3be25e69e982a3895d1ed6709aec30d6bb971df
arand4 <36410559+arand4@users.noreply.github.com>
2026-02-10 13:43:57 +0000

    Enable first-chance exception logging and heartbeat UI state logger for crash/freeze diagnostics

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Windows;
+using System.Diagnostics;
+using System.Threading;
 
 namespace TouchKeyboardMouse;
 
 public partial class App : Application
 {
+    private Timer? _heartbeatTimer;
     public App()
     {
         this.DispatcherUnhandledException += App_DispatcherUnhandledException;
         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+        _heartbeatTimer = new Timer(HeartbeatLog, null, 2000, 2000);
     }
 
     private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
@@ -23,6 +28,32 @@ public partial class App : Application
             TouchKeyboardMouse.Helpers.AppLogger.LogException(ex, "AppDomainUnhandledException");
         else
             TouchKeyboardMouse.Helpers.AppLogger.Log($"Unhandled non-Exception: {e.ExceptionObject}");
+    }
+
+    private void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+    {
+        TouchKeyboardMouse.Helpers.AppLogger.Log($"FirstChanceException: {e.Exception.Message}\n{e.Exception.StackTrace}");
+    }
+
+    private void HeartbeatLog(object? state)
+    {
+        try
+        {
+            var mainWindow = Current?.MainWindow as TouchKeyboardMouse.MainWindow;
+            if (mainWindow != null)
+            {
+                string info = $"Heartbeat: WindowState={mainWindow.WindowState}, NumpadVisible={mainWindow.NumpadPanel?.Visibility}, TrackpadWidth={mainWindow.Trackpad?.Width}, TrackpadActualWidth={mainWindow.Trackpad?.ActualWidth}, NumpadColumnWidth={mainWindow.NumpadColumn?.Width.Value}";
+                TouchKeyboardMouse.Helpers.AppLogger.Log(info);
+            }
+            else
+            {
+                TouchKeyboardMouse.Helpers.AppLogger.Log("Heartbeat: MainWindow not available");
+            }
+        }
+        catch (Exception ex)
+        {
+            TouchKeyboardMouse.Helpers.AppLogger.LogException(ex, "HeartbeatLog");
+        }
     }
 
     protected override void OnStartup(StartupEventArgs e)
